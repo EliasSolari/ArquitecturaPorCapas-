895d4b359dc1124f7f22cb524c621b1c47463b08
eliassolari <Eliasmartin_solari@hotmail.com>
2024-07-12 20:12:45 -0300

    IEPE- (ESOLARI) - feat  :

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import router from "./routes/index.js";
+import { connectMongoDB } from "./config/mongoDb.config.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import passport from "passport";
+import initializePassport from "./config/passport.config.js";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+connectMongoDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser("secreto"));
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: "mongodb+srv://admin:admin123456@e-commerce.vn9a3yh.mongodb.net/ecommerce",
+      ttl: 15,
+    }),
+    secret: "CodigoSecreto",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+initializePassport();
+
+
+app.use("/api", router);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Escuchando el servidor en el puerto${process.env.PORT}`);
+});
